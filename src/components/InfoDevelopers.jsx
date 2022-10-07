9b85a7948717e0630e2f7c4f71a58c616579749f
Martin Amante <martinjamante5@gmail.com>
2022-10-06 21:17:39 -0300

    Header terminado con css. Martin Amante

--- src/components/InfoDevelopers.jsx
+++ src/components/InfoDevelopers.jsx
@@ -3,17 +3,13 @@ import '../stylesheets/InfoDevelopers.css'
 function InfoDevelopers(props) {
     return (
         <>
-            
-
-            <div className="Desarrolladores">
-                <div className='info'>
+            <div className='info'>
+                <div className='propiedades'>
                 <img className='imagen' src={props.img} />
-                    <div className='propiedades'>
                     <h4>Nombre:{props.nombre}</h4>
                     <p>Edad:{props.Edad}</p>
                     <p>Intereses:{props.intereses}</p>
                     <p>GitHub:{props.GitHub}</p>
-                    </div>                       
                 </div>
             </div>
         </>
