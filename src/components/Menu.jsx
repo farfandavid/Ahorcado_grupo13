314b63387f29d95cb3432ac571185e2b217946d2
farfandavid <70915094+farfandavid@users.noreply.github.com>
2022-11-21 12:58:36 -0300

    Menu principal arreglado centrar boton

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,13 +9,16 @@ import { Link } from "react-router-dom";
 /**  Componente de funcion llamado "Menu", devuelve varios elementos y sera exportado hacia el componente "MenuPage"  */
 function Menu() {
     return (
-         /** fragment que permite devolver mas de un elemento, devuelve un <h2> con el nombre de nuestro grupo, una etiqueta <div> que es un boton que permite acceder al juego y un <h5>con el nombre de la materia. Hay algunas etiquetas div para poder darle diferentes estilos en CSS   */ 
+        /** fragment que permite devolver mas de un elemento, devuelve un <h2> con el nombre de nuestro grupo, una etiqueta <div> que es un boton que permite acceder al juego y un <h5>con el nombre de la materia. Hay algunas etiquetas div para poder darle diferentes estilos en CSS   */
         <>
             <div className='MenuEstilo'>
                 <h2>Grupo 13  </h2>
-                <div className='boton'><Link to="/Juegos">Jugar</Link></div>
+                <div className='centrar'>
+                    <div className='boton'><Link to="/Juegos">Jugar</Link></div>
+                </div>
                 <h5>Fundamentos de programacion web </h5>
             </div>
+
 
         </>
 
